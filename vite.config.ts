cfc0c7d47a43f889148a45f7c113b06feaca3a4d
MrUncleFish <a.muratov@emcd.io>
2026-06-10 01:47:59 +0500

    fix(pages): set vite base and router basename to /votitt/ for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import {VitePluginFonts} from 'vite-plugin-fonts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/votitt/',
     plugins: [
         react(),
         VitePluginFonts({
